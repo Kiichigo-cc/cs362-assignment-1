299b08f025f7981bb7c7b87195a971d7b0320951
Kiichigo-cc <oliveryzhou@gmail.com>
2024-04-09 14:22:38 -0700

    make asciiArt, add package-lock, and update package.json

--- asciiArt.js
+++ asciiArt.js
@@ -1,3 +1,12 @@
 const argv = process.argv;
 const stringInput = argv[2]
-console.log("User inputted string: \"" + stringInput + "\"");
+var figlet = require("figlet");
+
+figlet(stringInput, function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
